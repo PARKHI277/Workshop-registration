33103cd51b383ff57b0837cfd469f276b570f382
Parkhi <parkhigarg277@gmail.com>
2022-05-01 17:27:40 +0530

    length

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,66 +1,52 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-    Name:{
-        type:String,
-        required:true
-    },
-    Rollno:
-    {
-       type:String,
-       required:true,
-       maxlength:7,
-       minlength: 7,
-       unique:true
-    },
-    Contactno:
-    {
-        type:Number,
-        required:true,
-        maxlength:10,
-        minlength:10,
-        unique:true
-    },
-    Email:
-    {
-       type:String,
-       match: [
-        /[a-z0-9]+@akgec.ac.in/,
-        'Please add a valid email address.'
-      ],
-       required:'Email address is required',
-       lowercase:true.valueOf,
-       unique:true
-    },
-    Branch:
-    {
-        type:String,
-        required:true
-    },
-    Year:
-    {
-        type:Number,
-        required:true,
-    },
-    Gender:
-    {
-        type:String,
-        required:true
-    },
-    Residence:
-    {
-       type:String,
-       required : true
-    },
+  Name: {
+    type: String,
+    required: true,
+  },
+  Rollno: {
+    type: String,
+    required: true,
+    maxlength: 8,
+    minlength: 7,
+    unique: true,
+  },
+  Contactno: {
+    type: Number,
+    required: true,
+    maxlength: 10,
+    minlength: 10,
+    unique: true,
+  },
+  Email: {
+    type: String,
+    match: [/[a-z0-9]+@akgec.ac.in/, "Please add a valid email address."],
+    required: "Email address is required",
+    lowercase: true.valueOf,
+    unique: true,
+  },
+  Branch: {
+    type: String,
+    required: true,
+  },
+  Year: {
+    type: Number,
+    required: true,
+  },
+  Gender: {
+    type: String,
+    required: true,
+  },
+  Residence: {
+    type: String,
+    required: true,
+  },
 
-    token:{
-        type:String,
-        required:true
-    }
+  token: {
+    type: String,
+    required: true,
+  },
+});
 
-})
-
-
-
-
-const User = new mongoose.model('User',UserSchema);
-module.exports = User; 
+const User = new mongoose.model("User", UserSchema);
+module.exports = User;
